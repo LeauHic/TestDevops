81480c4183507c624186b01ab2feca2d7d1bf358
Loïck B <loickbrouard@yahoo.fr>
2023-06-14 15:58:55 +0200

    coucou

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Production
+          Développement
         </h1>
 
         <p className={styles.description}>
