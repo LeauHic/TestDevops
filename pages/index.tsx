b804afd195262084d93f7e73c12e9c65090232d2
Loïck B <loickbrouard@yahoo.fr>
2023-06-14 13:39:50 +0200

    email

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Bienvenue Watchtower
+          Bienvenue Email moi !!
         </h1>
 
         <p className={styles.description}>
